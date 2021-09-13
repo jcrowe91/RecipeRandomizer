02c3e8cfe1b016e75f946a27407b469ca6fe5155
jcrowe91 <jcrowe9119@gmail.com>
2021-09-12 21:48:01 -0500

    Add project files.

---
+++ RecipeRandomizer/RecipeBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeRandomizer
+{
+    public class RecipeBook
+    {
+        public string Name { get; set; }
+        public int Days { get; set; }
+
+
+        public RecipeBook(string name, int days)
+        {
+            Name = name;
+            Days = days;
+        }
+    }
+}
